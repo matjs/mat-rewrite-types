60431f73284a8856390b7eee22ad9039efb52912
chongzhi.ldq <chongzhi.ldq@alibaba-inc.com>
2019-04-11 11:04:35 +0800

    Fix: .ts等非js格式文件访问带参数出错

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ const path = require('path')
  * 优先顺序 mx > ts > es > js
  */
 function rewriteType(url, supportTypes = ['.mx', '.es', '.ts']) {
-
     for (let i = 0, len = supportTypes.length; i < len; i++) {
         let type = supportTypes[i]
-        let _url = url.replace(/\.js/g, type)
+        let paths = url.split('?')
+        let _url = paths[0].replace(/\.js$/g, type)
 
         if (fse.pathExistsSync(path.resolve(process.cwd(), _url.slice(1)))) {
-            return _url
+            return  _url
         }
     }
     return url
