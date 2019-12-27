b2fb022300f83429433f754908e4f3a83c6a4018
chongzhi.ldq <chongzhi.ldq@alibaba-inc.com>
2019-12-27 17:52:10 +0800

    Fix: cwd

--- index.js
+++ index.js
@@ -5,14 +5,16 @@ const path = require('path')
  * 默认支持.mx/.ts/.es文件
  * 优先顺序 mx > ts > es > js
  */
-function rewriteType(url, supportTypes = ['.mx', '.es', '.ts']) {
+function rewriteType(url, supportTypes = ['.mx', '.es', '.ts'], cwd) {
+    const _cwd = cwd || process.cwd()
+
     for (let i = 0, len = supportTypes.length; i < len; i++) {
         let type = supportTypes[i]
         let paths = url.split('?')
         let _url = paths[0].replace(/\.js$/g, type)
 
-        if (fse.pathExistsSync(path.resolve(process.cwd(), _url.slice(1)))) {
-            return  _url
+        if (fse.pathExistsSync(path.resolve(_cwd, _url.slice(1)))) {
+            return _url
         }
     }
     return url
